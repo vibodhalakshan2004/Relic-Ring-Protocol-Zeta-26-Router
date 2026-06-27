133fad31a0454d411b7fa5b1030edbb8accc4e18
vibodhalakshan2004 <vibodhalakshan@gmail.com>
2026-06-27 17:29:38 +0530

    Create models.py

---
+++ app/models.py
@@ -0,0 +1,7 @@
+from __future__ import annotations
+
+from dataclasses import asdict, dataclass
+from typing import Any, Optional
+
+from pydantic import BaseModel, Field, field_validator, model_validator
+
